126a3ecdef600508c7d2ca493199745f0ead950d
Sergio Lopez <sergio.2385z@gmail.com>
2021-09-29 20:38:53 -0500

    Adicion Capa Frontend

--- SeguimientoNutricional.App/SeguimientoNutricional.App.Dominio/Entidades/Persona.cs
+++ SeguimientoNutricional.App/SeguimientoNutricional.App.Dominio/Entidades/Persona.cs
@@ -1,4 +1,6 @@
 using System;
+using System.ComponentModel.DataAnnotations;
+
 
 namespace SeguimientoNutricional.App.Dominio
 {
@@ -6,12 +8,16 @@ namespace SeguimientoNutricional.App.Dominio
     {
         public int Id { get; set; }
 
+        [Required(ErrorMessage = "El Documento de Identidad es Obligatorio"), StringLength(15)]
         public string DocumentoIdentidad { get; set; }
 
+        [Required(ErrorMessage = "El Nombre es Obligatorio"), StringLength(15)]
         public string Nombre { get; set; }
 
+        [Required(ErrorMessage = "El Apellido es Obligatorio"), StringLength(15)] 
         public string Apellido { get; set; }
 
+        [Required(ErrorMessage = "El Telefono es Obligatorio"), StringLength(15)]
         public string Telefono { get; set; }
         
         public Genero Genero { get; set; }
