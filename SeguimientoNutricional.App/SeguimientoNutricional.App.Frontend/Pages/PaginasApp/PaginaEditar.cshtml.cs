d32f0977381ad177f23bf6ee0fa1b33d3409e4e4
Sergio Lopez <sergio.2385z@gmail.com>
2021-09-30 22:30:19 -0500

    CRUD 75% Paciente- Plantilla

--- SeguimientoNutricional.App/SeguimientoNutricional.App.Frontend/Pages/PaginasApp/PaginaEditar.cshtml.cs
+++ SeguimientoNutricional.App/SeguimientoNutricional.App.Frontend/Pages/PaginasApp/PaginaEditar.cshtml.cs
@@ -4,13 +4,49 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using SeguimientoNutricional.App.Persistencia;
+using SeguimientoNutricional.App.Dominio;
 
 namespace SeguimientoNutricional.App.Frontend.Pages.PaginasApp
 {
     public class PaginaEditarModel : PageModel
     {
-        public void OnGet()
+        private readonly IRepositorioPaciente _repoPaciente;
+
+        public PaginaEditarModel(IRepositorioPaciente _repoPaciente)
         {
+            this._repoPaciente = _repoPaciente;
+        }
+
+        [BindProperty]
+
+        public Paciente Paciente { get; set; }
+
+        
+        public IActionResult OnGet(int pacienteId)
+        {
+            Paciente =_repoPaciente.GetPaciente(pacienteId);
+            if (Paciente == null)
+            { 
+                return RedirectToPage("./NotFound");
+            }
+            else
+            { 
+                return Page();
+            }
+        }
+
+        public IActionResult OnPost()
+        {
+            if(!ModelState.IsValid)
+            {
+                return Page();
+            }
+            else
+            { 
+                Paciente = _repoPaciente.UpdatePaciente(Paciente);
+            }
+            return RedirectToPage("./PaginaPaciente");
         }
     }
 }
